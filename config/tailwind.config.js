94b398f4072f56c087250a600bc25299542a49eb
Hrishi Mittal <hrishimittal@gmail.com>
2024-07-04 18:24:28 +0100

    Add shopify starter kit theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        palette: {
+          lighter: '#F5F3FF',
+          light: '#DDD6FE',
+          primary: '#5B21B6',
+          dark: '#4C1D95',
+        },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
