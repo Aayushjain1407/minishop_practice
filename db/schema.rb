14b5e7017ba5fa53b53749d6e86cde5279ab5fff
Hrishi Mittal <hrishimittal@gmail.com>
2024-09-06 17:03:32 +0100

    Add Stripe Price ID field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_162627) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_06_160231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_18_162627) do
     t.bigint "seller_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_price_id"
     t.index ["seller_id"], name: "index_products_on_seller_id"
   end
 
